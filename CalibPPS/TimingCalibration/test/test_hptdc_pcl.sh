46f449aa2634d3dc111b088acfccfe5ba5ffb681
Christopher <krzysztof.misan@cern.ch>
2022-10-08 13:44:27 +0200

    fix test

--- CalibPPS/TimingCalibration/test/test_hptdc_pcl.sh
+++ CalibPPS/TimingCalibration/test/test_hptdc_pcl.sh
@@ -3,7 +3,11 @@
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
-TEST_DIR=src/CalibPPS/TimingCalibration/test
+TEST_DIR=$CMSSW_RELEASE_BASE/src/CalibPPS/TimingCalibration/test
+
+if [ -e $CMSSW_BASE/src/CalibPPS/TimingCalibration/test ] ; then
+   TEST_DIR=$CMSSW_BASE/src/CalibPPS/TimingCalibration/test
+fi
 
 cmsRun  $TEST_DIR/DiamondCalibrationWorker_cfg.py || die "HPTDC PCL failed at worker stage" $?
 echo "HPTDC PCL worker succeeded"
